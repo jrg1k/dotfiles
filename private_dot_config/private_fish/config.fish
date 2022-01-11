2d2ba0c4771ea264f6b42f741d8a6c34e0a59d70
jk <jk@jkristensen.net>
2022-01-12 00:07:27 +0100

    Update private_dot_config/private_fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,9 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if test -n "$DESKTOP_SESSION"
-    set -gx MOZ_ENABLE_WAYLAND 1
-    set -gx QT_QPA_PLATFORM wayland
-    set -gx SDL_VIDEODRIVER wayland
+if status is-login
     set -x (gnome-keyring-daemon --start | string split "=")
 end
