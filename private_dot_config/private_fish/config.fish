e15206d661cd428476abae8d9cd4fd214b545f70
jk <jk@jkristensen.net>
2022-01-11 19:17:44 +0100

    Update private_dot_config/private_fish/config.fish Update private_dot_config/sway/config Update private_dot_config/waybar/style.css

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,4 +6,6 @@ if test -n "$DESKTOP_SESSION"
     set -gx MOZ_ENABLE_WAYLAND 1
     set -gx QT_QPA_PLATFORM wayland
     set -gx SDL_VIDEODRIVER wayland
+    set -gx XDG_CURRENT_DESKTOP sway
+    set -x (gnome-keyring-daemon --start | string split "=")
 end
