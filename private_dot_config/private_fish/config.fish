880f1e1d63289f5ac1a42ebc310724841a44d48c
jk <jk@jkristensen.net>
2022-01-12 00:00:11 +0100

    Update private_dot_config/private_fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,4 +6,5 @@ if test -n "$DESKTOP_SESSION"
     set -gx MOZ_ENABLE_WAYLAND 1
     set -gx QT_QPA_PLATFORM wayland
     set -gx SDL_VIDEODRIVER wayland
+    set -x (gnome-keyring-daemon --start | string split "=")
 end
