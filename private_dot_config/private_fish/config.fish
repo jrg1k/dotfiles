5e71a7c5e213d590e1e42f5acab8f2f79f5e7fb9
jk <jk@laptop>
2022-01-18 00:47:57 +0100

    Update private_dot_config/private_fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,11 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if test -n "$DESKTOP_SESSION"
-    set -gx MOZ_ENABLE_WAYLAND 1
-    set -gx QT_QPA_PLATFORM wayland
-    set -gx SDL_VIDEODRIVER wayland
-end
-
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme gruvbox
