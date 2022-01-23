4632d8d8cef6a3f0aa8654f67d93c60038f1a88c
jk <jk@desktop>
2022-01-23 11:48:12 +0100

    Update private_dot_config/private_fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if status is-login
+    set -x (gnome-keyring-daemon --start | string split "=")
+end
+
 set -gx MOZ_ENABLE_WAYLAND 1
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme gruvbox
