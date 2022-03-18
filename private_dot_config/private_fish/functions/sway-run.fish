a26d39131017a0909755a13602e4c3d6a39e0fe6
Jørgen Kristensen <jk@bitnor.org>
2022-03-18 18:56:47 +0100

    Update private_dot_config/private_fish/functions/sway-run.fish

--- private_dot_config/private_fish/functions/sway-run.fish
+++ private_dot_config/private_fish/functions/sway-run.fish
@@ -1,6 +1,9 @@
 function sway-run
   set -gx MOZ_ENABLE_WAYLAND 1
-  set -gx QT_QPA_PLATFORM wayland
+  set -gx QT_QPA_PLATFORM wayland-egl
+  set -gx QT_WAYLAND_FORCE_DPI physical
+  set -gx ECORE_EVAS_ENGINE wayland_egl
+  set -gx ELM_ENGINE wayland_egl
   set -gx SDL_VIDEODRIVER wayland
   set -gx XDG_CURRENT_DESKTOP sway
   sway
