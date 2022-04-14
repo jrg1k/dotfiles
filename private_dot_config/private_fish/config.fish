fdd5a5cc3f3eea87a6620a8c3ba224fd423ae27d
Jørgen Kristensen <jk@bitnor.org>
2022-04-14 22:21:03 +0200

    Update private_dot_config/private_fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,2 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme dark
+set -g theme_color_scheme base16-dark
